c680832bf9e11f2d66333279913d33ddbfa3a46a
LayMui <mui_ume@hotmail.com>
2025-06-14 19:14:18 +0800

    toggle switch

--- src/App.js
+++ src/App.js
@@ -1,89 +1,21 @@
-import "./App.css";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as yup from "yup";
-import { validateEmail } from "./utils";
-const schema = yup.object().shape({  
-  firstName: yup.string().required("First name is required"),
-  lastName: yup.string(), 
-  email: yup
-    .string()
-    .email("Invalid email")
-    .required("Email is required")
-    .test("validateEmail", "Invalid email", (value) => validateEmail(value)),
-  password: yup
-    .string()
-    .min(8, "Password must be at least 8 characters")
-    .required("Password is required"),
-  role: yup
-    .string()
-    .notOneOf(['role'], "Please select a valid role")
-    .required("Role is required"),
-});
+// AppRouter.jsx or App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Registration from "./Registration";
+import Welcome from "./Welcome";
+import { ThemeProvider } from "./ThemeContext";
 
-// Password Error Component (Optionally)
-const PasswordErrorMessage = (msg) => {
-  return <p className="FieldError">{msg.children}</p>;
-};
 
-function App() {
+function AppRouter() {
   return (
-    <div className="App">
-      <Formik
-        initialValues={{ firstName: "", lastName: "", email: "", password: "", role: "role" }}
-        validationSchema={schema}
-        onSubmit={(values, { resetForm }) => {
-          alert("Account created!");
-          resetForm();
-        }}>
-        {({ isValid, dirty }) => (
-          <Form>
-            <fieldset>
-              <h2>Sign Up</h2>
-
-              <div className="Field">
-                <label>First name <sup>*</sup></label>
-                <Field name="firstName" placeholder="First name" />
-                <ErrorMessage name="firstName" component="p" className="FieldError" />
-              </div>
-
-              <div className="Field">
-                <label>Last name</label>
-                <Field name="lastName" placeholder="Last name" />
-                <ErrorMessage name="lastName" component="p" className="FieldError" />
-              </div>
-
-              <div className="Field">
-                <label>Email address <sup>*</sup></label>
-                <Field name="email" placeholder="Email address" />
-                <ErrorMessage name="email" component="p" className="FieldError" />
-              </div>
-
-              <div className="Field">
-                <label>Password <sup>*</sup></label>
-                <Field name="password" type="password" placeholder="Password" />
-                <ErrorMessage name="password" render={(msg) => <PasswordErrorMessage>{msg}</PasswordErrorMessage>} />
-              </div>
-
-              <div className="Field">
-                <label>Role <sup>*</sup></label>
-                <Field as="select" name="role">
-                    <option value="role">Select a role</option>
-                    <option value="individual">Individual</option>
-                    <option value="business">Business</option>
-                </Field>
-                <ErrorMessage name="role" component="p" className="FieldError" />
-              </div>
-
-              <button type="submit" disabled={!isValid || !dirty}>
-                Create account
-              </button>
-            </fieldset>
-          </Form>
-        )}
-
-      </Formik>
-    </div>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Registration />} />
+          <Route path="/welcome" element={<Welcome />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default AppRouter;
---
+++ src/Registration.js
@@ -0,0 +1,97 @@
+import "./Registration.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+import { validateEmail } from "./utils";
+import { useNavigate } from "react-router-dom";
+
+
+const schema = yup.object().shape({  
+  firstName: yup.string().required("First name is required"),
+  lastName: yup.string(), 
+  email: yup
+    .string()
+    .email("Invalid email")
+    .required("Email is required")
+    .test("validateEmail", "Invalid email", (value) => validateEmail(value)),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
+  role: yup
+    .string()
+    .notOneOf(['role'], "Please select a valid role")
+    .required("Role is required"),
+});
+
+// Password Error Component (Optionally)
+const PasswordErrorMessage = (msg) => {
+  return <p className="FieldError">{msg.children}</p>;
+};
+
+
+function Registration() {
+  const navigate = useNavigate();
+  return (
+    <div className="App">
+      <Formik
+        initialValues={{ firstName: "", lastName: "", email: "", password: "", role: "role" }}
+        validationSchema={schema}
+        onSubmit={(values, { resetForm }) => {
+        //  alert("Account created!");
+         
+          resetForm();
+            // Redirect to the new page
+            navigate("/welcome");
+        }}>
+        {({ isValid, dirty }) => (
+          <Form>
+            <fieldset>
+              <h2>Sign Up</h2>
+
+              <div className="Field">
+                <label>First name <sup>*</sup></label>
+                <Field name="firstName" placeholder="First name" />
+                <ErrorMessage name="firstName" component="p" className="FieldError" />
+              </div>
+
+              <div className="Field">
+                <label>Last name</label>
+                <Field name="lastName" placeholder="Last name" />
+                <ErrorMessage name="lastName" component="p" className="FieldError" />
+              </div>
+
+              <div className="Field">
+                <label>Email address <sup>*</sup></label>
+                <Field name="email" placeholder="Email address" />
+                <ErrorMessage name="email" component="p" className="FieldError" />
+              </div>
+
+              <div className="Field">
+                <label>Password <sup>*</sup></label>
+                <Field name="password" type="password" placeholder="Password" />
+                <ErrorMessage name="password" render={(msg) => <PasswordErrorMessage>{msg}</PasswordErrorMessage>} />
+              </div>
+
+              <div className="Field">
+                <label>Role <sup>*</sup></label>
+                <Field as="select" name="role">
+                    <option value="role">Select a role</option>
+                    <option value="individual">Individual</option>
+                    <option value="business">Business</option>
+                </Field>
+                <ErrorMessage name="role" component="p" className="FieldError" />
+              </div>
+
+              <button type="submit" disabled={!isValid || !dirty}>
+                Create account
+              </button>
+            </fieldset>
+          </Form>
+        )}
+
+      </Formik>
+    </div>
+  );
+}
+
+export default Registration;
